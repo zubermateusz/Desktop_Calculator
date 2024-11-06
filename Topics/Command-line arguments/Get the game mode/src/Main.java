7c47341812580c7056b31795b1d96e0f7565d2d5
Mateusz <zubermateusz@gmail.com>
2024-11-06 15:37:49 +0100

    Add loop for search "mode"

--- Topics/Command-line arguments/Get the game mode/src/Main.java
+++ Topics/Command-line arguments/Get the game mode/src/Main.java
@@ -2,7 +2,17 @@ import java.util.Scanner;
 
 class Problem {
     public static void main(String[] args) {
-
+        boolean isModeSetValue = false;
+        for (int i = 0; i < args.length; i+=2) {
+            if (args[i].contains("mode")) {
+                System.out.println(args[i+1]);
+                isModeSetValue = true;
+                break;
+            }
+        }
+        if (!isModeSetValue) {
+            System.out.println("default");
+        }
 
     }
 }
