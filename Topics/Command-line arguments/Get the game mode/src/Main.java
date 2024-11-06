93b71d26eb7c0cb023645a9d32f67a3be06369ec
Mateusz <zubermateusz@gmail.com>
2024-11-06 15:15:26 +0100

    Add Scanner to read line from user

--- Topics/Command-line arguments/Get the game mode/src/Main.java
+++ Topics/Command-line arguments/Get the game mode/src/Main.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 class Problem {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
     }
 }
