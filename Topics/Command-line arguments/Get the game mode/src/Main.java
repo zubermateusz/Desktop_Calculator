108fc4386b0787f926fef3cf0566594ced9d153c
Mateusz <zubermateusz@gmail.com>
2024-11-06 15:22:08 +0100

    Deleted scanner

--- Topics/Command-line arguments/Get the game mode/src/Main.java
+++ Topics/Command-line arguments/Get the game mode/src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 class Problem {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String line = scanner.nextLine();
+
+
     }
 }
