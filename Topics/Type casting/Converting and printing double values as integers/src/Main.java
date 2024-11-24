86f4cf2485cf97f48a7a3f1d9fc72d4f9d1a2458
Mateusz <zubermateusz@gmail.com>
2024-11-24 22:22:23 +0100

    Initial commit

---
+++ Topics/Type casting/Converting and printing double values as integers/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Read the number as a double.
+        // TODO: Write your code here to read a double from the console
+
+        // Convert and print the number to an integer.
+
+        // TODO: Write your code here to convert the double to an integer and print it out to the console
+
+        scanner.close();
+    }
+}
