1be97ee199c665f6c9b2526fff28edefdfb2f3a6
Mateusz <zubermateusz@gmail.com>
2024-11-24 21:57:44 +0100

    Initial commit

---
+++ Topics/Defining classes/Creating a book class and displaying book details/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String title = sc.nextLine();
+        String author = sc.nextLine();
+        Book book = new Book();
+
+        // Set the details of the book
+        // Implement here
+
+        // Print the details of the book
+        // Implement here
+    }
+}
+
+class Book {
+    private String title;
+    private String author;
+    private int pages;
+
+    // Getters and setters
+    // Implement here
+}
