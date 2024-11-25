2ccc89ed3bc8fc108b77a33bcbeb2533c2be7997
Mateusz <zubermateusz@gmail.com>
2024-11-25 14:16:02 +0100

    Add new task You need to create a basic hierarchy of classes in Java to represent different types of Animals. In this system, animals have a name and a method called sound which returns the sound the animal makes. A Dog is an animal, and it barks. A Cat on the other hand meows. You will receive an animal type and a name as inputs. Depending on the animal type, instantiate the appropriate animal object with the given name and print out its sound. If the animal type does not exist in your system, just print out 'Animal type not recognized'.

---
+++ Topics/Object class/Generating sounds based on animal types/src/Main.java
@@ -0,0 +1,28 @@
+// Java code template starts here
+import java.util.*;
+
+public class Main {
+
+    // Define Animal class here
+
+    // Define Dog class here
+
+    // Define Cat class here
+
+    public static void main(String[] args) {
+
+        // In this section, you need to add code to receive input
+        // and create the corresponding animal object
+
+        // Replace the placeholders with the appropriate calls
+        if(/*Dog?*/) {
+
+        } else if(/*Cat?*/) {
+
+        } else {
+            System.out.println("Animal type not recognized");
+        }
+    }
+} 
+
+// Java code template ends here
