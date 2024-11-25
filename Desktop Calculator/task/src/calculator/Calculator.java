aff1dd2e2410db98b19a0b6f9b2dabec9841697b
Mateusz <zubermateusz@gmail.com>
2024-11-25 21:20:42 +0100

    add button "1"

--- Desktop Calculator/task/src/calculator/Calculator.java
+++ Desktop Calculator/task/src/calculator/Calculator.java
@@ -15,21 +15,35 @@ public class Calculator extends JFrame {
 
         JTextField equationTextField = new JTextField("EquationTextField");
         equationTextField.setName("EquationTextField");
-        equationTextField.setBounds(50, 50, 200, 30);
+        equationTextField.setBounds(50, 10, 200, 30);
         equationTextField.setText("");
         equationTextField.setVisible(true);
         add(equationTextField);
 
         JButton button7 = new JButton("7");
         button7.setName("7");
-        button7.setBounds(50, 100, 50, 50);
+        button7.setBounds(20, 70, 45, 45);
         button7.setBackground(Color.lightGray);
         button7.setVisible(true);
         add(button7);
 
+        JButton button4 = new JButton("4");
+        button4.setName("4");
+        button4.setBounds(20, 130, 45, 45);
+        button4.setBackground(Color.lightGray);
+        button4.setVisible(true);
+        add(button4);
+
+        JButton button1 = new JButton("1");
+        button1.setName("1");
+        button1.setBounds(20, 190, 45, 45);
+        button1.setBackground(Color.lightGray);
+        button1.setVisible(true);
+        add(button1);
+
         JButton solve = new JButton("Solve");
         solve.setName("Solve");
-        solve.setBounds(70, 150, 100, 50);
+        solve.setBounds(70, 320, 100, 50);
         solve.setBackground(Color.lightGray);
         solve.setVisible(true);
         add(solve);
