c6c2470d5d7de4ca353de037466b4e9d3014f79d
Mateusz <zubermateusz@gmail.com>
2024-11-25 19:58:26 +0100

    You should add a single JButton and a single JTextField to the main program window. A click on the button adds both numbers in the JTextField and displays the result in the format 2+2 => 2+2=4, where 2+2 is the equation, and 2+2=4 is the result.
        In this project, we are not going to use any layout managers, so it's up to you to customize the calculator look.
    
        Objectives
        In this stage, you should:
    
        Set the title of the window as Calculator;
        Add a JTextField to the main window;
        Add a JButton to the main window;
        Set the names of the following components:
        JTextField as EquationTextField;
        JButton as Solve.
    
        Done

--- Desktop Calculator/task/src/calculator/Calculator.java
+++ Desktop Calculator/task/src/calculator/Calculator.java
@@ -1,6 +1,7 @@
 package calculator;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Calculator extends JFrame {
 
@@ -9,5 +10,33 @@ public class Calculator extends JFrame {
         setSize(300, 400);
         setLayout(null);
         setVisible(true);
+        setName("Calculator");
+        setTitle("Calculator");
+
+        JTextField equationTextField = new JTextField("EquationTextField");
+        equationTextField.setName("EquationTextField");
+        equationTextField.setBounds(70, 50, 100, 50);
+        equationTextField.setText("");
+        equationTextField.setVisible(true);
+        add(equationTextField);
+
+
+        JButton solve = new JButton("Solve");
+        solve.setName("Solve");
+        solve.setBounds(70, 150, 100, 50);
+        solve.setBackground(Color.lightGray);
+        solve.setVisible(true);
+        add(solve);
+
+        solve.addActionListener(actionEvent -> {
+            equationTextField.getText();
+            equationTextField.setText(Calculator.getResult(equationTextField.getText()));
+        });
+
+    }
+
+    private static String getResult(String text) {
+        int result = Integer.parseInt(String.valueOf(text.charAt(0))) + Integer.parseInt(String.valueOf(text.charAt(2)));
+        return text + "=" + result;
     }
 }
