c9a7abaec92b1f281b1d6e2a3bb4fbbf45a6243b
Mateusz <zubermateusz@gmail.com>
2024-11-25 14:38:24 +0100

    Done

--- Topics/Object class/Generating sounds based on animal types/src/Main.java
+++ Topics/Object class/Generating sounds based on animal types/src/Main.java
@@ -4,24 +4,46 @@ import java.util.*;
 public class Main {
 
     // Define Animal class here
+    private static class Animal {
+        public String sound;
+        public String sound(){
+            return this.sound;
+        }
+
+    }
 
     // Define Dog class here
+    public static class Dog extends Animal {
+        public Dog() {
+            this.sound = "Bark!";
+        }
+
+    }
 
     // Define Cat class here
+    public static class Cat extends Animal {
+        public Cat() {
+            this.sound = "Meow!";
+        }
+    }
 
     public static void main(String[] args) {
 
         // In this section, you need to add code to receive input
         // and create the corresponding animal object
-
+        Scanner scanner = new Scanner(System.in);
+        String animal = scanner.nextLine();
+        Dog dog = new Dog();
+        Cat cat = new Cat();
         // Replace the placeholders with the appropriate calls
-        if(/*Dog?*/) {
-
-        } else if(/*Cat?*/) {
-
+        if(animal.contains("Dog")) {
+            System.out.println(dog.sound());
+        } else if(/*Cat?*/animal.contains("Cat")) {
+            System.out.println(cat.sound());
         } else {
             System.out.println("Animal type not recognized");
         }
+        scanner.close();
     }
 } 
 
