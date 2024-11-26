ddb4d0eed39b8255b8759e94dc85702891bad525
Mateusz <zubermateusz@gmail.com>
2024-11-26 18:18:16 +0100

    There is an abstract class Calculator:
    abstract class Calculator {
    
        public abstract long sum(long val1, long val2);
    
        public abstract long subtraction(long val1, long val2);
    }
    You should create an anonymous class that extends the given class and assign the instance to the variable anonymousCalculator.
    
    The anonymous class must override both abstract methods of the class. The method sum should return the sum of its arguments. The method subtraction should return the difference between the first and second arguments.

---
+++ Topics (1)/Anonymous classes properties/Calculator/src/Main.java
@@ -0,0 +1,11 @@
+class CalculatorWrapper {
+    static Calculator anonymousCalculator = /* create an instance of an anonymous class here, 
+                                            do not forget ; in the end */
+}
+
+abstract class Calculator {
+
+    public abstract long sum(long val1, long val2);
+
+    public abstract long subtraction(long val1, long val2);
+}
