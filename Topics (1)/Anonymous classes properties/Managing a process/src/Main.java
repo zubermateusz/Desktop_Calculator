55da1a432f30a413d12cd1f9ea6ad3dfbcdf91f0
Mateusz <zubermateusz@gmail.com>
2024-11-26 18:14:51 +0100

    Done

--- Topics (1)/Anonymous classes properties/Managing a process/src/Main.java
+++ Topics (1)/Anonymous classes properties/Managing a process/src/Main.java
@@ -14,17 +14,21 @@ public class Main {
         startLongProcess(new Callback() {
             @Override
             public void onStarted() {
-
+                System.out.println("The process started");
             }
 
             @Override
             public void onStopped(String cause) {
-
+                System.out.println(cause);
             }
 
             @Override
             public void onFinished(int code) {
-
+                if (code == 0) {
+                    System.out.println("The process successfully finished");
+                } else {
+                    System.out.println("The process is finished with error: " + code);
+                }
             }
         });
 
