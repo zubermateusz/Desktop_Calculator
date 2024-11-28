f20690a2a0f94a40044174240e948922dde9f73f
Mateusz <zubermateusz@gmail.com>
2024-11-28 15:39:56 +0100

    Done

--- Topics (1)/Processing strings/Modifying a string by changing letters and digits/src/Main.java
+++ Topics (1)/Processing strings/Modifying a string by changing letters and digits/src/Main.java
@@ -3,7 +3,25 @@ import java.util.Scanner;
 public class Main {
     public static String transformString(String s) {
         // implement your string processing here
-        return "";
+        String sAfterChanged = "";
+        for (char c : s.toCharArray()) {
+            if (c > '0' && c <= '9') {
+                sAfterChanged += --c;
+                continue;
+            }
+            if (c == '0') {
+                sAfterChanged += "9";
+                continue;
+            }
+            if (c >= 'a' && c < 'z') {
+                sAfterChanged += ++c;
+                continue;
+            }
+            if (c == 'z') {
+                sAfterChanged += "a";
+            }
+        }
+        return sAfterChanged;
     }
 
     public static void main(String[] args) {
