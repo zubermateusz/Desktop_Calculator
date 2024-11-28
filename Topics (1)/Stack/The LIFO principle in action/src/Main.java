fa4018978e6f8d62808420d836d510a4e9fa4935
Mateusz <zubermateusz@gmail.com>
2024-11-28 17:09:18 +0100

    Done

--- Topics (1)/Stack/The LIFO principle in action/src/Main.java
+++ Topics (1)/Stack/The LIFO principle in action/src/Main.java
@@ -1,5 +1,20 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
         // put your code here
+        Deque<Integer> stack = new ArrayDeque();
+        Scanner scanner = new Scanner(System.in);
+        int numberOfElements = scanner.nextInt();
+        while (numberOfElements > 0) {
+            stack.push(scanner.nextInt());
+            numberOfElements--;
+        }
+        scanner.close();
+        for (int n : stack) {
+            System.out.println(stack.pop());
+        }
     }
 }
