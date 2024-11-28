050a1de390f3f3d4416f655e493dad00e757813f
Mateusz <zubermateusz@gmail.com>
2024-11-28 15:10:25 +0100

    In Java programming, you are given a string which includes only lowercase English letters and digits. Your task is to modify the string with the following rules: If it's a letter, change it to the next letter in the alphabet. If it's a digit, change it to the previous digit. If the character is '0', change it to '9'. If the character is 'z', change it to 'a'. You should return the modified string.

---
+++ Topics (1)/Processing strings/Modifying a string by changing letters and digits/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static String transformString(String s) {
+        // implement your string processing here
+        return "";
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String s = input.nextLine();
+        System.out.println(transformString(s));
+    }
+}
