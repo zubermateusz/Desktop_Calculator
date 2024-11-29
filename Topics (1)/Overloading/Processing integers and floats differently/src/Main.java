f1f44f961d2e60f3a2ecd5eed802e34a27f633bc
Mateusz <zubermateusz@gmail.com>
2024-11-29 15:46:43 +0100

    Done

--- Topics (1)/Overloading/Processing integers and floats differently/src/Main.java
+++ Topics (1)/Overloading/Processing integers and floats differently/src/Main.java
@@ -4,9 +4,19 @@ public class Main {
 
     // Create a method named 'process' to handle integers
     // Hint: When dealing with integers, you should return the square of the number
-
+    public void process(Integer number) {
+        System.out.println(number * number);
+    }
     // Create a method named 'process' to handle floats
     // Hint: When dealing with floats, you should return the number rounded to the nearest whole number
+    public void process(Float number) {
+        if (number.intValue() + 0.5 >= number) {
+            System.out.println(number.intValue());
+        } else {
+            System.out.println(number.intValue() + 1);
+        }
+
+    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -17,9 +27,11 @@ public class Main {
         try {
             Integer integerInput = Integer.parseInt(input);
             // Call the 'process' method with the integer input and print the result
+            new Main().process(integerInput);
         } catch (NumberFormatException e) {
             Float floatInput = Float.parseFloat(input);
             // Call the 'process' method with the float input and print the result
+            new Main().process(floatInput);
         }
     }
 }
