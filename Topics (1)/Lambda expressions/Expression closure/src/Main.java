b9a2629d4f0402022b7d147b4f3a6920109d8fcc
Mateusz <zubermateusz@gmail.com>
2024-11-30 14:57:55 +0100

    Using closure write a lambda expression that calculates a∗x2+b∗x+c  where a, b, c are context final variables. They will be available in the context during testing. Note, the result is double.
     Done

---
+++ Topics (1)/Lambda expressions/Expression closure/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.function.DoubleUnaryOperator;
+
+class Operator {
+
+    public static int a = 10;
+    public static int b = 20;
+    public static int c = 30;
+
+    public static DoubleUnaryOperator unaryOperator = (x) -> { return a * x * x + b * x + c;};
+
+    // Write your code here
+}
